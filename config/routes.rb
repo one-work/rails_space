7fe8d61255caeb403af4341e00925a19487a5ebe
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-11 20:50:48 +0800

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,74 +1,72 @@
 Rails.application.routes.draw do
-  scope RailsCom.default_routes_scope do
-    namespace :space, defaults: { business: 'space' } do
-      resources :grids, only: [] do
-        member do
-          get :qrcode
+  namespace :space, defaults: { business: 'space' } do
+    resources :grids, only: [] do
+      member do
+        get :qrcode
+      end
+    end
+    resources :desks, only: [] do
+      member do
+        get :qrcode
+      end
+    end
+    resources :rooms, only: [] do
+      member do
+        get :qrcode
+      end
+    end
+    resources :stations do
+      resources :desks do
+        collection do
+          post :change
         end
       end
-      resources :desks, only: [] do
-        member do
-          get :qrcode
-        end
-      end
-      resources :rooms, only: [] do
-        member do
-          get :qrcode
-        end
-      end
+    end
+    namespace :admin, defaults: { namespace: 'admin' } do
+      root 'home#index'
       resources :stations do
-        resources :desks do
-          collection do
-            post :change
+        resources :buildings
+        resources :rooms do
+          member do
+            get :print_data
           end
-        end
-      end
-      namespace :admin, defaults: { namespace: 'admin' } do
-        root 'home#index'
-        resources :stations do
-          resources :buildings
-          resources :rooms do
+          resources :grids do
             member do
               get :print_data
             end
-            resources :grids do
-              member do
-                get :print_data
-              end
-            end
-            resources :desks do
-              member do
-                get :print_data
-                post :print
-                post :print_all
-              end
-            end
           end
-        end
-        resources :rooms, only: [] do
-          collection do
-            get :all
-          end
-        end
-        resources :desks, only: [] do
-          collection do
-            get :all
+          resources :desks do
+            member do
+              get :print_data
+              post :print
+              post :print_all
+            end
           end
         end
       end
-      namespace :me, defaults: { namespace: 'me' } do
-        resources :rooms do
+      resources :rooms, only: [] do
+        collection do
+          get :all
+        end
+      end
+      resources :desks, only: [] do
+        collection do
+          get :all
+        end
+      end
+    end
+    namespace :me, defaults: { namespace: 'me' } do
+      resources :rooms do
+        member do
+          post :in
+          post :out
+          get :qrcode
+        end
+        resources :grids do
           member do
             post :in
             post :out
             get :qrcode
-          end
-          resources :grids do
-            member do
-              post :in
-              post :out
-              get :qrcode
-            end
           end
         end
       end
