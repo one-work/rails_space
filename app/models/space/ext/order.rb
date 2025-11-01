8ad6951ca0d4aca7d0b401d4f1e48c5e5f5d4ac5
qinmingyuan <mingyuan0715@foxmail.com>
2025-11-01 20:45:33 +0800

    update counters logic

--- app/models/space/ext/order.rb
+++ app/models/space/ext/order.rb
@@ -6,16 +6,22 @@ module Space
       belongs_to :desk, class_name: 'Space::Desk', optional: true
       belongs_to :station, class_name: 'Space::Station', optional: true
 
+      after_save :update_counters_to_desk, if: -> { saved_change_to_unreceived_amount? }
       after_create :increment_counts_to_desk
       after_destroy :decrement_counts_to_desk
     end
 
+    def update_counters_to_desk
+      r = unreceived_amount - unreceived_amount_before_last_save
+      desk.update_json_counter(unreceived_amount: r)
+    end
+
     def increment_counts_to_desk
-      desk.update_json_counter(amount: amount, count: 1)
+      desk.update_json_counter(count: 1, unreceived_amount: unreceived_amount)
     end
 
     def decrement_counts_to_desk
-      desk.update_json_counter(amount: -amount, count: -1)
+      desk.update_json_counter(count: -1)
     end
 
   end
