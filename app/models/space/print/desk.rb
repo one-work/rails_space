f21fcf1e9e441d0e250e646b94ce48614ee9362f
qinmingyuan <mingyuan0715@foxmail.com>
2026-04-20 22:53:44 +0800

    desk

--- app/models/space/print/desk.rb
+++ app/models/space/print/desk.rb
@@ -2,35 +2,34 @@ module Space
   module Print::Desk
     extend ActiveSupport::Concern
 
-    def to_esc(pr)
+    def to_esc(pr, aim: 'order')
       total = 0
 
-      pr.text_big "#{organ.name}"
-      pr.break_line
-      pr.text "#{self.class.human_attribute_name(:name)}：#{name}"
-      pr.text '已下单：'
-      trade_items.where(status: 'ordered').each do |item|
-        total += item.amount
-        pr.text(" #{item.good_name} #{item.number.to_human} x #{item.single_price.to_money.to_s}") if item.good
+      case aim
+      when 'order'
+        pr.text_big "#{organ.name}"
+        pr.break_line
+        pr.text "#{self.class.human_attribute_name(:name)}：#{name}"
+        pr.text '已下单：'
+        trade_items.where(status: 'ordered').each do |item|
+          total += item.amount
+          pr.text(" #{item.good_name} #{item.number.to_human} x #{item.single_price.to_money.to_s}") if item.good
+        end
+        pr.break_line
+        pr.text "合计：#{total.to_money.to_s}"
+        pr.break_line
+        pr.text "感谢您的惠顾！"
+        pr.text "订餐电话：#{'0717-6788808'}"
+        pr.text "#{Time.current.to_fs(:wechat)}"
+      when 'xx'
+        pr.bar(height: 20)
+        pr.qrcode(product_url, x: 20, y: 10, cell_width: 10)
+        pr.text(name, x: 320, scale: 2)
+        pr.middle_text('扫码点餐', x: 320)
+      when 'xxx'
+        pr.text code
+        pr.qrcode_right(product_url)
       end
-      pr.break_line
-      pr.text "合计：#{total.to_money.to_s}"
-      pr.break_line
-      pr.text "感谢您的惠顾！"
-      pr.text "订餐电话：#{'0717-6788808'}"
-      pr.text "#{Time.current.to_fs(:wechat)}"
-    end
-
-    def to_tspl(ts)
-      ts.bar(height: 20)
-      ts.qrcode(product_url, x: 20, y: 10, cell_width: 10)
-      ts.text(name, x: 320, scale: 2)
-      ts.middle_text('扫码点餐', x: 320)
-    end
-
-    def to_cpcl(cpcl)
-      cpcl.text code
-      cpcl.qrcode_right(product_url)
     end
 
   end
