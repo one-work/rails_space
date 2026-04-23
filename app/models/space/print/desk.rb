4d53349e65a78b9adccee58baed377503abd3230
qinmingyuan <mingyuan0715@foxmail.com>
2026-04-23 15:30:21 +0800

    simple

--- app/models/space/print/desk.rb
+++ app/models/space/print/desk.rb
@@ -24,8 +24,8 @@ module Space
       when 'dinner'
         pr.dash(height: 20)
         pr.qrcode_right(product_url)
-        pr.text(name, x: 320)
-        pr.text_big_center('扫码点餐', x: 320)
+        pr.text(name)
+        pr.text('扫码点餐')
       when 'xxx'
         pr.text code
         pr.qrcode_right(product_url)
