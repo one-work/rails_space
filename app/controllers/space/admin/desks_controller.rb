313fe94723fa24ccbcd9d1afd46caba5ce6982c9
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-07 11:53:07 +0800

    support name search

--- app/controllers/space/admin/desks_controller.rb
+++ app/controllers/space/admin/desks_controller.rb
@@ -9,10 +9,14 @@ module Space
     end
 
     def all
+      q_params = {}
+      q_params.merge! default_params
+      q_params.merge! params.permit('name-like')
+
       station = Station.default_where(default_params).first || Station.create(default_params)
       @room = Room.default_where(default_params).first || Room.create(station_id: station.id, **default_params)
 
-      @desks = Desk.default_where(default_params).page(params[:page])
+      @desks = Desk.default_where(q_params).page(params[:page])
       @item_hash = Trade::Item.default_where(default_params).where.not(desk_id: nil).carting.group(:desk_id).count
       @ordered_hash = Trade::Item.default_where(default_params).where.not(desk_id: nil).status_ordered.group(:desk_id).count
     end
