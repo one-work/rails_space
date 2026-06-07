da888f99e1a0cf028133102e0977808c24af4158
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-07 10:11:52 +0800

    fix

--- app/models/space/print/desk.rb
+++ app/models/space/print/desk.rb
@@ -11,15 +11,18 @@ module Space
         pr.break_line
         pr.text "#{self.class.human_attribute_name(:name)}：#{name}"
         pr.text '已下单：'
-        trade_items.where(status: 'ordered').each do |item|
-          total += item.amount
-          pr.text(" #{item.good_name} #{item.number.to_human} x #{item.single_price.to_money.to_s}") if item.good
+        orders.where(state: 'init').each do |order|
+          total += order.amount
+          order.items.each do |item|
+            pr.text(" #{item.good_name} #{item.number.to_human} x #{item.single_price.to_money.to_s}") if item.good
+          end
         end
         pr.break_line
         pr.text "合计：#{total.to_money.to_s}"
         pr.break_line
-        pr.text "感谢您的惠顾！"
-        pr.text "订餐电话：#{'0717-6788808'}"
+        organ.print_note.to_s.split("\n").each do |note|
+          pr.text note
+        end
         pr.text "#{Time.current.to_fs(:wechat)}"
       when 'dinner'
         pr.dash(height: 20)
