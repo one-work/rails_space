53ec1452677d605b508d7705e01ed4d9f44e07e6
qinmingyuan <mingyuan0715@foxmail.com>
2026-09-07 16:40:38 +0800

    sep paid and unpaid

--- app/models/space/print/desk.rb
+++ app/models/space/print/desk.rb
@@ -3,16 +3,31 @@ module Space
     extend ActiveSupport::Concern
 
     def to_esc(pr, aim: 'order')
-      total = 0
-
       case aim
       when 'order'
         pr.text_big_center "#{organ.name}"
         pr.dash
         pr.text "#{self.class.human_attribute_name(:name)}：#{name}"
         pr.dash
+
+        pr.text '已下单（已支付）：'
+        pr.dash
+        paid = 0
+        paids = []
+        orders.where(state: 'init', payment_status: 'paid').each do |order|
+          paid += order.amount
+          order.items.each do |item|
+            paids << [item.good_name, item.single_price.to_money.to_s, item.number.to_human, item.amount.to_money.to_s]
+          end
+        end
+        pr.table_3(cols: paids)
+        pr.dash
+        pr.text "合计（已支付）：#{paid.to_money.to_s}"
+        pr.dash
+
         pr.text '已下单（未支付）：'
         pr.dash
+        total = 0
         cols = []
         orders.where(state: 'init', payment_status: 'unpaid').each do |order|
           total += order.amount
@@ -23,8 +38,10 @@ module Space
         pr.table_3(cols: cols)
         pr.break_line
         pr.dash
-        pr.text "合计：#{total.to_money.to_s}"
+        pr.text "合计（未支付）：#{total.to_money.to_s}"
         pr.break_line
+        pr.dash
+
         organ.print_note.to_s.split("\n").each do |note|
           pr.text note
         end
