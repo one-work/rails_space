43aea6171e9ce1c83e7bc66a76f85a9d2c2485b0
qinmingyuan <mingyuan0715@foxmail.com>
2026-09-09 15:02:48 +0800

    cache ordr logic

--- app/models/space/ext/order.rb
+++ app/models/space/ext/order.rb
@@ -6,7 +6,7 @@ module Space
       belongs_to :desk, class_name: 'Space::Desk', optional: true
       belongs_to :station, class_name: 'Space::Station', optional: true
 
-      after_save :update_counters_to_desk, if: -> { desk_id.present? && saved_change_to_unreceived_amount? }
+      after_save :update_counters_to_desk, if: -> { desk_id.present? && (['unreceived_amount', 'state'] & saved_changes.keys).present? }
       after_create :increment_counts_to_desk, if: -> { desk_id.present? }
       after_destroy :decrement_counts_to_desk, if: -> { desk_id.present? }
     end
@@ -14,12 +14,25 @@ module Space
     def update_counters_to_desk
       return unless desk
       r = unreceived_amount - unreceived_amount_before_last_save
-      desk.update_json_counter(unreceived_amount: r)
+      if state == 'done' && state_before_last_save != 'done'
+        sr = -1
+      else
+        sr = 0
+      end
+
+      desk.update_json_counter(
+        unreceived_amount: r,
+        undo: sr
+      )
     end
 
     def increment_counts_to_desk
       return unless desk
-      desk.update_json_counter(count: 1, unreceived_amount: unreceived_amount)
+      desk.update_json_counter(
+        count: 1,
+        undo: 1,
+        unreceived_amount: unreceived_amount
+      )
     end
 
     def decrement_counts_to_desk
